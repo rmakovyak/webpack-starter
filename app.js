2464c9881728913e66fc10a116028423a0504767
Roman Makovyak <roman.makovyak@savinggglobal.com>
2016-10-28 16:00:56 +0200

    backbone and basic webpack config

---
+++ app.js
@@ -0,0 +1,10 @@
+document.write('this is the app');
+
+// require('./app/class');
+require('./styles/styles');
+
+if (__DEV__) {
+  console.warn('Extra logging');
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// Modules
+var webpack = require('webpack');
+
+/**
+* definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
+*/
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  __RELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_RELEASE || 'false')),
+});
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        filename: './public/bundle.js'       
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                  presets: ['es2015']
+                }
+            },
+            {
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader'
+            },
+            { 
+                test: /\.(png|jpg)$/, 
+                /**
+                 * inline base64 URLs for <=8k images, direct URLs for the rest
+                 */
+                loader: 'url-loader?limit=8192' 
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.css'] 
+    },
+    plugins: [definePlugin]     
+};
